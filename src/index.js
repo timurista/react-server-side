b679bd8f43b6176baa0c539ed044923461aa2de4
timurista <timothy.urista@gmail.com>
2017-11-05 12:59:26 -0700

    added webpack node externals which will not build the entire node modules for the server index.js file, since they are not needed except on client and built with the second process

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express  from 'express';
-import React from 'react';
+import React from 'react'; // will pull everthing for webpack
 import { renderToString } from 'react-dom/server';
 import Home from './client/components/Home';
 const app = express();
--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base');
+// webpack says anything inside node modules file will not be included
+// inside the node modules bundle
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
   // Inform webpack we are building bundle for node js
@@ -9,7 +12,8 @@ const config = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  }
+  },
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
