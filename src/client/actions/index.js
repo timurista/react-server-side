77d85f43a3cccf002e3b78a6b5fe2f856f78aa5a
timurista <timothy.urista@gmail.com>
2017-11-06 08:21:24 -0700

    add reducers and base action get users using the api provided

---
+++ src/client/actions/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+// https://react-ssr-api.herokuapp.com/
+export const FETCH_USERS = 'fetch_users';
+export const fetchUsers = () => async dispatch => {
+  const res = await axios.get('https://react-ssr-api.herokuapp.com/users');
+  dispatch({
+    type: FETCH_USERS,
+    payload: res
+  });
+}
